2b6fe26491d0b3b8832f7e7525fa2af4a653c26c
beemergrom <139361626+beemergrom@users.noreply.github.com>
2023-07-12 16:18:03 +0200

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+class DBTest(unittest.TestCase):
+    def test_create_table(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+    def test_select_definition(self):
+        pass
+
+    def test_edit_definition(self):
+        pass
+
+    def test_append_definition(self):
+        pass
+
+    def test_select_all(self):
+        pass
+
+    def test_select_n_random(self):
+        pass
